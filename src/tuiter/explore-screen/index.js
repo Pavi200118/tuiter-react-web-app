aff91190b6152587c0d73660ff7b4ea1bf03e283
Pavi200118 <pavisowmi01@gmail.com>
2023-05-29 12:59:14 -0400

    JavaScript and React.js Assignment 3

--- src/tuiter/explore-screen/index.js
+++ src/tuiter/explore-screen/index.js
@@ -29,12 +29,7 @@ function ExploreScreen() {
        <li className="nav-item">
          <a className="nav-link">News</a>
        </li>
-       <li className="nav-item">
-                <a className="nav-link">Sports</a>
-              </li>
-              <li className="nav-item">
-                       <a className="nav-link">Entertainment</a>
-                     </li>
+
      </ul>
      <div className="position-relative mb-2">
        <img src={starshipImage} className="w-100"/>
