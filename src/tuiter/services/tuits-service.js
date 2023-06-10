0988f537f255294f61b972e5b6496c9fea5ee6d5
Pavi200118 <pavisowmi01@gmail.com>
2023-06-10 13:21:06 -0400

    a5

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 const API_BASE = process.env.REACT_APP_API_BASE;
+
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
@@ -9,7 +10,7 @@ const response = await axios.post(TUITS_API, tuit)
 
 export const deleteTuit = async (tid) => {
   const response = await axios.delete(`${TUITS_API}/${tid}`)
-  return response.data
+  return response.data;
 }
 
 export const updateTuit = async (tuit) => {
