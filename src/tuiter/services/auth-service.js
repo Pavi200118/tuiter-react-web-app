d37baca08b24865e55428b5488b9d3ceb0dac88f
Pavi200118 <pavisowmi01@gmail.com>
2023-06-12 14:13:43 -0400

    a5

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -17,6 +17,7 @@ export const logout = async () => {
 };
 export const profile = async () => {
  const response = await api.post(`${USERS_URL}/profile`);
+ console.log(response.data)
  return response.data;
 };
 
